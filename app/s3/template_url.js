c0c7a4deee8d780cb78612fc2dcc953120cb6f77
Augusto Toledo <atoledo@pcorp.cl>
2019-06-04 15:07:31 -0400

    Resolves #13

--- app/s3/template_url.js
+++ app/s3/template_url.js
@@ -3,8 +3,8 @@ const meta = require("../stack_metadata");
 
 module.exports = async () => {
   const stackData = await meta.getFileData();
-  //http://bucket.s3.amazonaws.com
+  const template = stackData.template ? stackData.template : "main.yml";
   return `https://${stackData.bucket}.s3.amazonaws.com/${stackData.domain}/${
     stackData.name
-  }/${stackData.version}/main.yml`;
+  }/${stackData.version}/${template}`;
 };
