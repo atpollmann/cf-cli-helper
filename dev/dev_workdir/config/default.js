ae277d7415286348fa1a649ce45b1cc03436dc3a
Augusto Toledo <atoledo@pcorp.cl>
2019-08-18 00:07:37 -0400

    Fix #19: update project scaffolding files

--- dev/dev_workdir/config/default.js
+++ dev/dev_workdir/config/default.js
@@ -3,6 +3,8 @@ module.exports = {
     credentialsProfile: "default",
     region: "us-east-1",
     pollingInterval: 1,
-    onCreateStackFailure: "ROLLBACK"
+    onCreateStackFailure: "ROLLBACK",
+    template: "main.yml",
+    capabilities: ["CAPABILITY_IAM"]
   }
 };
