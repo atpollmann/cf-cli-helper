e3a29d781aa297261f5e21ab7e95309790b18e80
Augusto Toledo <atoledo@pcorp.cl>
2019-12-26 14:56:10 -0300

    Metadata passed as parameters

--- app/template_parameters/get_parameters.js
+++ app/template_parameters/get_parameters.js
@@ -2,8 +2,17 @@ const file = require("../util/file");
 const parameters_file = require("./parameters_file");
 const { AppError, commonErrors } = require("../errors");
 const environment = require("../environment");
+const metadata = require("../stack_metadata");
 
 let paramsCache = null;
+
+async function getMetadata() {
+  try {
+    return await metadata.getFileData();
+  } catch (e) {
+    throw e;
+  }
+}
 
 async function getFileContents() {
   try {
@@ -36,7 +45,8 @@ module.exports = exports = async () => {
     try {
       environmentObj = { Environment: environment.get() };
       let preParams = await parseParams(fileContents);
-      params = paramsCache = { ...environmentObj, ...preParams };
+      let meta = await getMetadata();
+      params = paramsCache = { ...environmentObj, ...meta, ...preParams };
     } catch (e) {
       throw new AppError(commonErrors.badFormat, e);
     }
--- app/template_parameters/template_parameters.test.js
+++ app/template_parameters/template_parameters.test.js
@@ -7,6 +7,10 @@ const config = require("../util/config");
 
 const valid_params_parsed = {
   Environment: "dev",
+  Name: "Test-VPC",
+  Domain: "global",
+  Version: "1.6",
+  Bucket: "my-test-bucket",
   VPCCIDR: "10.0.0.0/16",
   AZ1: "us-east-1a",
   AZ2: "us-east-1b",
@@ -18,7 +22,7 @@ const valid_params_parsed = {
   PublicSubnetAZ2CIDR: "10.0.96.0/20",
   PublicSubnetAZ3CIDR: "10.0.160.0/20"
 };
-const valid_params_file = `{"metadata": {} , "parameters": {"VPCCIDR": "10.0.0.0/16","AZ1": "us-east-1a","AZ2": "us-east-1b","AZ3": "us-east-1c","PrivateSubnetAZ1CIDR":"10.0.0.0/19","PrivateSubnetAZ2CIDR": "10.0.64.0/19","PrivateSubnetAZ3CIDR": "10.0.128.0/19","PublicSubnetAZ1CIDR": "10.0.32.0/20","PublicSubnetAZ2CIDR": "10.0.96.0/20","PublicSubnetAZ3CIDR": "10.0.160.0/20"}}`;
+const valid_params_file = `{"metadata": {"Name": "Test-VPC","Domain": "global","Version": "1.6","Bucket": "my-test-bucket"} , "parameters": {"VPCCIDR": "10.0.0.0/16","AZ1": "us-east-1a","AZ2": "us-east-1b","AZ3": "us-east-1c","PrivateSubnetAZ1CIDR":"10.0.0.0/19","PrivateSubnetAZ2CIDR": "10.0.64.0/19","PrivateSubnetAZ3CIDR": "10.0.128.0/19","PublicSubnetAZ1CIDR": "10.0.32.0/20","PublicSubnetAZ2CIDR": "10.0.96.0/20","PublicSubnetAZ3CIDR": "10.0.160.0/20"}}`;
 const invalid_params_file = `{"metadata": {} , "parameters": {"VPCCIDR": "10.0.0.0/16","AZ1": "us-east-1a","AZ2": "us-east-1b","AZ3": "us-east-1c","PrivateSubnetAZ1CIDR":"10.0.0.0/19","PrivateSubnetAZ2CIDR": "10.0.64.0/19" "PrivateSubnetAZ3CIDR": "10.0.128.0/19",PublicSubnetAZ1CIDR: 10.0.32.0/20,"PublicSubnetAZ2CIDR": "10.0.96.0/20","PublicSubnetAZ3CIDR": "10.0.160.0/20"}}`;
 
 describe("Template parameters module", () => {
@@ -76,6 +80,20 @@ describe("Template parameters module", () => {
       setFileContents(valid_params_file);
       const params = await parameters.getParameters();
       expect(params).toHaveProperty("Environment", "dev");
+    });
+    test("Metadata must be passed as parameters to the template", async () => {
+      expect.assertions(4);
+      config.setConfig({
+        environment: "dev",
+        "aws.onCreateStackFailure": "ROLLBACK",
+        "aws.capabilities": []
+      });
+      setFileContents(valid_params_file);
+      const params = await parameters.getParameters();
+      expect(params).toHaveProperty("Name");
+      expect(params).toHaveProperty("Domain");
+      expect(params).toHaveProperty("Version");
+      expect(params).toHaveProperty("Bucket");
     });
   });
 });
