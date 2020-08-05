119dde8d40a4d3967f85b2a64d0a1cd40aa8c9a1
Augusto Toledo <atoledo@pcorp.cl>
2020-08-04 20:41:55 -0400

    Fix failing test

--- app/stack_metadata/get_file_data.js
+++ app/stack_metadata/get_file_data.js
@@ -3,25 +3,20 @@ const file = require("../util/file");
 const metaFile = require("./metadata_file");
 const { AppError, commonErrors } = require("../errors");
 
-let metaCache = null;
-
 module.exports = exports = async () => {
-  let metadata = metaCache;
+  let metadata = null;
 
-  if (metadata === null) {
-    await file
-      .readFromFile(metaFile)
-      .then(data => {
-        metadata = JSON.parse(data).metadata;
-        metaCache = metadata;
-      })
-      .catch(e => {
-        throw new AppError(
-          commonErrors.cantReadFile,
-          "Could not write to stage file: " + e
-        );
-      });
-  }
+  await file
+    .readFromFile(metaFile)
+    .then(data => {
+      metadata = JSON.parse(data).metadata;
+    })
+    .catch(e => {
+      throw new AppError(
+        commonErrors.cantReadFile,
+        "Could not write to stage file: " + e
+      );
+    });
 
   return metadata;
 };
